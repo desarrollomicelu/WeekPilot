2fb7494319fd18a70438e0acd489e82156426eb8
desarrollomicelu <auxdesarrollo@micelu.co>
2025-03-12 12:36:20 -0500

    Revert "Rama dm"

--- extensions.py
+++
@@ -1,6 +0,0 @@
-# extensions.py
-from flask_bcrypt import Bcrypt
-from flask_sqlalchemy import SQLAlchemy
-
-bcrypt = Bcrypt()
-db = SQLAlchemy()
--- routes/dashboard.py
+++
@@ -1,7 +0,0 @@
-from flask import Blueprint, render_template
-
-dashboard_bp = Blueprint("dashboard", __name__, template_folder="templates")
-
-@dashboard_bp.route("/dashboard", endpoint="dashboard")
-def dashboard():
-    return render_template("dashboard.html")
